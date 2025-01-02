8b1f5e18be9bc067dc3bf252b510abf0c1aab5d9
NoSoussaille <marwane.belhadi@hotmail.com>
2025-01-02 10:18:34 +0100

    Initialisation du projet Rendezvous

---
+++ elements/header.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'session'
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1 class="title">Rendez-Vous</h1>
+    </header>
+    
+</body>
+</html>
